5c9d7b3018a40db3b1b97d4687b22cea84338620
Prateek Singh Saini <prateek.saini@austere.co.in>
2025-04-19 20:09:28 +0530

    first commit

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,45 +1,108 @@
-import { Tabs } from 'expo-router';
 import React from 'react';
-import { Platform } from 'react-native';
+import { Tabs } from 'expo-router';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import Feather from 'react-native-vector-icons/Feather';
 
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+const tabScreens = [
+    {
+        name: 'index',
+        focusedIcon: {
+            Component: Ionicons,
+            name: 'home',
+        },
+        unfocusedIcon: {
+            Component: Ionicons,
+            name: 'home',
+        },
+    },
+    {
+        name: 'search',
+        focusedIcon: {
+            Component: Feather,
+            name: 'search',
+        },
+        unfocusedIcon: {
+            Component: Feather,
+            name: 'search',
+        },
+    },
+    {
+        name: 'saved',
+        focusedIcon: {
+            Component: AntDesign,
+            name: 'star',
+        },
+        unfocusedIcon: {
+            Component: AntDesign,
+            name: 'staro',
+        },
+    },
+    {
+        name: 'profile',
+        focusedIcon: {
+            Component: FontAwesome,
+            name: 'user-circle',
+        },
+        unfocusedIcon: {
+            Component: FontAwesome,
+            name: 'user-circle-o',
+        },
+    },
+];
 
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
+const _Layout = () => {
+    return (
+        <Tabs
+            screenOptions={{
+                tabBarShowLabel: false,
+                tabBarItemStyle: {
+                    width: '100%',
+                    height: "100%",
+                    justifyContent: 'center'
+                },
+                tabBarStyle: {
+                    position: 'absolute',
+                    bottom: 40,
+                    width: '70%',
+                    marginLeft: 55,
+                    backgroundColor: 'white',
+                    height: 60,
+                    borderRadius: 20,
+                    paddingTop: 10,
+                    paddingBottom: 10,
+                    shadowColor: '#000',
+                    shadowOpacity: 0.1,
+                    shadowRadius: 10,
+                    elevation: 5,
+                }
 
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
-    </Tabs>
-  );
-}
+            }}>
+            {tabScreens.map(({ name, focusedIcon, unfocusedIcon }) => (
+                <Tabs.Screen
+                    key={name}
+                    name={name}
+                    options={{
+                        headerShown: false,
+                        tabBarIcon: ({ focused }) => {
+                            const { Component, name } = focused
+                                ? focusedIcon
+                                : unfocusedIcon;
+
+                            return (
+                                <Component
+                                    name={name}
+                                    size={24}
+                                    color={focused ? 'black' : 'gray'}
+                                />
+                            );
+                        },
+                    }}
+                />
+            ))}
+        </Tabs>
+    );
+};
+
+export default _Layout;
---
+++ components/SearchBar.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import Feather from 'react-native-vector-icons/Feather';
+
+const SearchBar = () => {
+  return (
+    <View>
+      <Feather />
+    </View>
+  )
+}
+
+export default SearchBar
